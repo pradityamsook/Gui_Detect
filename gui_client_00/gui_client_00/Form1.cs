1de5952ef3c1f17583538b05df0d93f425fa2512
pradityamsook <praditcry@gmail.com>
2018-06-06 09:18:35 +0700

    fixed read URL from camera to textbox

--- gui_client_00/gui_client_00/Form1.cs
+++ gui_client_00/gui_client_00/Form1.cs
@@ -16,24 +16,26 @@ using Emgu.CV.Structure;
 
 namespace gui_client_00
 {
+
+
     public partial class Form1 : Form
     {
-        string var;
+        public string var;
 
-        VideoCapture videoPlay;
-        Image<Bgr, byte> imgInput;
+        private VideoCapture videoPlay;
         private FileConfig ini = new FileConfig(@"C:\Users\Leon\Documents\_config\config_position.ini");
 
         private bool _canDraw, Box, Line1, Line2, Line3; 
         public bool _DrawLine1, _DrawLine2, _DrawLine3;
         private int _startX, _startY, _endX, _endY, _line1Y, _line2Y, _line3Y;
         private Rectangle _rect;
-       
+
+        private Mat _frame = new Mat();
+        private bool _captureProcess;
 
         public int x, y, width, height, widthOfLine;
 
         
-
         public int x1, y1;
 
         
@@ -41,46 +43,57 @@ namespace gui_client_00
         public Form1()
         {
             InitializeComponent();
+            CvInvoke.UseOpenCL = false;
+       
         }
 
-        private async void button1_Click(object sender, EventArgs e)
+        private void Process(object sender, EventArgs e)
+        {
+            if (videoPlay != null && videoPlay.Ptr != IntPtr.Zero)
+            {
+                videoPlay.Retrieve(_frame, 0);
+                pictureBox1.Image = _frame.Bitmap;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
             try
             {
-                while (true)
-                {
-                    Mat m = new Mat();
-                    videoPlay.Read(m);
-
-                    if (!m.IsEmpty)
-                    {
-                        pictureBox1.Image = m.Bitmap;
-                        double fps = videoPlay.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps);
-                        await Task.Delay(1000 / Convert.ToInt32(fps));
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
+                videoPlay.ImageGrabbed += Process;
             }
-            catch (Exception ex)
+            catch (NullReferenceException ex)
             {
                 MessageBox.Show(ex.Message);
             }
+
+            if (videoPlay != null)
+            {
+                if (_captureProcess)
+                {
+                    button1.Text = "Start Capture";
+                    videoPlay.Pause();
+                }
+                else
+                {
+                    button1.Text = "Stop Capture";
+                    videoPlay.Start();
+                }
+                _captureProcess = !_captureProcess;
+            }
+
         }
 
         /* This function is update of video play location of video and will send this update to button1_Click about event*/
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            
             var = textBox1.Text;
-            //imgInput = new Image<Bgr, byte>(var);
             videoPlay = new VideoCapture(var);
-            Mat m = new Mat();
-
-            videoPlay.Read(m);
-            pictureBox1.Image = m.Bitmap;
+            if (videoPlay != null && videoPlay.Ptr != IntPtr.Zero)
+            {
+                videoPlay.Retrieve(_frame, 0);
+                pictureBox1.Image = _frame.Bitmap;
+            }
         }
         /*****************************************************************************************************************/
 
@@ -126,31 +139,43 @@ namespace gui_client_00
 
         private void pictureBox1_paint(object sender, PaintEventArgs e)
         {
-            ///////////////////////////////////////////////////////////////////////
+            ///////Update Box paint////////////////////////////////////////////////
             using (Pen pen = new Pen(Color.Blue, 5))
             {
                 e.Graphics.DrawRectangle(pen, _rect);
             }
             //////////////////////////////////////////////////////////////////////
 
-            //////////////////////////////////////////////////////////////////////
+            ///////Update Line1 paint////////////////////////////////////////////
             using (Pen pen = new Pen(Color.Red, 5))
             {
                 e.Graphics.DrawLine(pen, _startX, _line1Y, widthOfLine, _line1Y);
+                ini.Write("Position Line1", "X1", Convert.ToString(_startX));
+                ini.Write("Position Line1", "Y1", Convert.ToString(_line1Y));
+                ini.Write("Position Line1", "X2", Convert.ToString(widthOfLine));
+                ini.Write("Position Line1", "Y2", Convert.ToString(_line1Y));
             }
             //////////////////////////////////////////////////////////////////////
 
-            //////////////////////////////////////////////////////////////////////
+            ///////Update Line2 paint/////////////////////////////////////////////
             using (Pen pen = new Pen(Color.Green, 5))
             {
                 e.Graphics.DrawLine(pen, _startX, _line2Y, widthOfLine, _line2Y);
+                ini.Write("Position Line2", "X1", Convert.ToString(_startX));
+                ini.Write("Position Line2", "Y1", Convert.ToString(_line2Y));
+                ini.Write("Position Line2", "X2", Convert.ToString(widthOfLine));
+                ini.Write("Position Line2", "Y2", Convert.ToString(_line2Y));
             }
             //////////////////////////////////////////////////////////////////////
 
-            //////////////////////////////////////////////////////////////////////
+            ///////Update Line3 paint/////////////////////////////////////////////
             using (Pen pen = new Pen(Color.Azure, 5))
             {
                 e.Graphics.DrawLine(pen, _startX, _line3Y, widthOfLine, _line3Y);
+                ini.Write("Position Line3", "X1", Convert.ToString(_startX));
+                ini.Write("Position Line3", "Y1", Convert.ToString(_line3Y));
+                ini.Write("Position Line3", "X2", Convert.ToString(widthOfLine));
+                ini.Write("Position Line3", "Y2", Convert.ToString(_line3Y));
             }
             //////////////////////////////////////////////////////////////////////
         }
@@ -183,8 +208,9 @@ namespace gui_client_00
                 _line3Y = e.Y;
             }
 
-            label1.Text = "" + Math.Min(_startX, e.X) + ", " + Math.Min(_startY, e.Y);
+            
             Refresh();
+            label1.Text = "" + Math.Min(_startX, e.X) + ", " + Math.Min(_startY, e.Y);
         }
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
